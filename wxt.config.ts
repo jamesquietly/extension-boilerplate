be4aa8d24ff160cae5d90f27c5d2844a8e591458
James Ly <jamesquietly@gmail.com>
2025-05-21 17:39:20 -0700

    change manifest.json

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react']
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'extension-boilerplate',
+    description: 'description for manifest.json',
+    host_permissions: ['https://*/*', 'http://*']
+  }
 });
